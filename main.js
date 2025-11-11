814b425b139e0e6bd268707776ca2f7ee49faff9
Manuel Pichiri <manuk95@live.it>
2025-11-11 17:03:52 +0100

    Esercizio Completo

---
+++ main.js
@@ -0,0 +1,75 @@
+/*1. Cos’è ReactJS?
+
+Domanda aperta
+React è una libreria che usa dei componenti riutilizzabili per velocizzare la creazione di pagine web.☑️
+
+2. ReactJS è un framework, non una libreria.
+
+Vero / Falso
+
+FALSO☑️
+
+3. Il file package.json contiene…
+
+– Informazioni non essenziali, può essere tranquillamente eliminato
+
+– Solamente il nome e la versione dell’applicazione
+
+– Molte informazioni utili, come ad esempio l’elenco di tutte le dipendenze richieste dall’applicazione   <---☑️
+
+– Il primo componente React caricato all’avvio
+
+4. create-react-app è l’unico modo possibile per creare un’applicazione React.
+
+Vero / Falso
+
+FALSO
+
+5. Qual è il comando da lanciare nel terminale per creare una nuova create-react-app con nome “test”?
+
+– npx create-react-app test
+
+– npm create-react-app test  <---☑️
+
+– npx create-react-app-test
+
+– npx create-new-react-app test
+
+6. Cos’è un componente React?
+
+– Una libreria che è necessario importare dentro l’applicazione
+
+– Una pagina ben specifica all’interno dell’applicazione
+
+– Un blocco di logica/contenuto riutilizzabile all’interno dell’applicazione  <--- ☑️
+
+– Una landing page per l’applicazione
+
+7. Un componente React può venire creato in tre modi: come funzione, classe o interfaccia.
+
+Vero / Falso
+
+VERO ☑️
+
+8. Le props sono frammenti di informazione assegnati all’invocazione di un componente React, utili al fine di rendere il componente dinamico e più riutilizzabile.
+
+Vero/Falso
+
+VERO☑️
+
+9. Le props possono essere passate solamente da un componente genitore ad un componente figlio, non è possibile fare il contrario 
+
+Vero/Falso
+
+VERO ☑️
+
+10. Da dove possono venire recuperate le props all’interno di un componente React creato come classe?
+
+– Sono proprietà contenute in un oggetto accessibile attraverso i parametri della funzione 
+
+– Possono essere recuperate all’interno dello oggetto ‘this’, dentro un sotto-oggetto chiamato ‘props’ <--- ☑️
+
+– Vengono applicate come proprietà dell’oggetto globale ‘window’
+
+– Vengono applicate come proprietà dell’oggetto ‘document’
+*/
